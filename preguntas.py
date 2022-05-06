6829d305722a0fd953564aee7f3a42138c686e38
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-05-06 11:11:46 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -71,7 +71,7 @@ def pregunta_01():
     df = df.drop(columns="veil_type")
 
     # Asigne la columna `type` a la variable `y`.
-    y = df["type"].values
+    y = df["type"]
 
     # Asigne una copia del dataframe `df` a la variable `X`.
     X = df.copy(deep=True)
